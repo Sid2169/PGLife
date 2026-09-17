08b37015cd64f99ed6af0a724d7efbaa95c6cde4
Sid2169 <siddharthasingh21109572@gmail.com>
2026-09-17 09:58:43 +0530

    style: refresh shared design system and accessible controls

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,10 @@
-<div class="header sticky-top">
-    <nav class="navbar navbar-expand-md navbar-light">
+<a class="skip-link" href="#main-content">Skip to main content</a>
+<header class="header sticky-top">
+    <nav class="navbar navbar-expand-md navbar-light" aria-label="Main navigation">
         <a class="navbar-brand" href="index.php">
-            <img src="img/logo.png" />
+            <img src="img/logo.png" alt="PG Life home" />
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#my-navbar">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#my-navbar" aria-controls="my-navbar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -13,37 +14,37 @@
                 if (!isset($_SESSION["user_id"])) {
                 ?>
                     <li class="nav-item">
-                        <a class="nav-link" href="#" data-toggle="modal" data-target="#signup-modal">
-                            <i class="fas fa-user"></i>Signup
+                        <a class="nav-link nav-signup" href="#" data-toggle="modal" data-target="#signup-modal">
+                            <i class="fas fa-user" aria-hidden="true"></i>Signup
                         </a>
                     </li>
-                    <div class="nav-vl"></div>
+                    <li class="nav-vl" aria-hidden="true"></li>
                     <li class="nav-item">
                         <a class="nav-link" href="#" data-toggle="modal" data-target="#login-modal">
-                            <i class="fas fa-sign-in-alt"></i>Login
+                            <i class="fas fa-sign-in-alt" aria-hidden="true"></i>Login
                         </a>
                     </li>
                 <?php
                 } else {
                 ?>
-                    <div class="nav-name">
+                    <li class="nav-name">
                         Hi, <?php echo e($_SESSION["full_name"]); ?>
-                    </div>
+                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="dashboard.php">
-                            <i class="fas fa-user"></i>Dashboard
+                            <i class="fas fa-user" aria-hidden="true"></i>Dashboard
                         </a>
                     </li>
-                    <div class="nav-vl"></div>
+                    <li class="nav-vl" aria-hidden="true"></li>
                     <li class="nav-item">
                         <a class="nav-link" href="logout.php">
-                            <i class="fas fa-sign-out-alt"></i>Logout
+                            <i class="fas fa-sign-out-alt" aria-hidden="true"></i>Logout
                         </a>
                     </li>
                 <?php
                 }
                 ?>
-                <div class="nav-vl"></div>
+                <li class="nav-vl" aria-hidden="true"></li>
                 <li class="nav-item">
                     <button type="button" class="nav-link theme-toggle" id="theme-toggle"
                         aria-pressed="false" aria-label="Toggle light or dark theme"
@@ -54,7 +55,8 @@
             </ul>
         </div>
     </nav>
-</div>
+</header>
 
-<div id="loading">
+<div id="loading" role="status" aria-live="polite">
+    <span class="sr-only">Loading, please wait.</span>
 </div>
--- includes/signup_modal.php
+++ includes/signup_modal.php
@@ -1,8 +1,8 @@
 <div class="modal fade" id="signup-modal" tabindex="-1" role="dialog" aria-labelledby="signup-heading" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="signup-heading">Signup with PGLife</h5>
+                <h2 class="modal-title" id="signup-heading">Signup with PGLife</h2>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -11,61 +11,65 @@
             <div class="modal-body">
                 <form id="signup-form" class="form" role="form" method="post" action="api/signup_submit.php">
                     <input type="hidden" name="csrf_token" value="<?= e(csrf_token()) ?>">
+                    <label for="signup-name">Full Name</label>
                     <div class="input-group form-group">
                         <div class="input-group-prepend">
                             <span class="input-group-text">
-                                <i class="fas fa-user"></i>
+                                <i class="fas fa-user" aria-hidden="true"></i>
                             </span>
                         </div>
-                        <input type="text" class="form-control" name="full_name" placeholder="Full Name" maxlength="30" required>
+                        <input type="text" class="form-control" id="signup-name" name="full_name" placeholder="Full Name" autocomplete="name" maxlength="30" required>
                     </div>
 
+                    <label for="signup-phone">Phone Number</label>
                     <div class="input-group form-group">
                         <div class="input-group-prepend">
                             <span class="input-group-text">
-                                <i class="fas fa-phone-alt"></i>
+                                <i class="fas fa-phone-alt" aria-hidden="true"></i>
                             </span>
                         </div>
-                        <input type="text" class="form-control" name="phone" placeholder="Phone Number" maxlength="10" minlength="10" required>
+                        <input type="text" class="form-control" id="signup-phone" name="phone" placeholder="Phone Number" autocomplete="tel" inputmode="tel" maxlength="10" minlength="10" required>
                     </div>
 
+                    <label for="signup-email">Email</label>
                     <div class="input-group form-group">
                         <div class="input-group-prepend">
                             <span class="input-group-text">
-                                <i class="fas fa-envelope"></i>
+                                <i class="fas fa-envelope" aria-hidden="true"></i>
                             </span>
                         </div>
-                        <input type="email" class="form-control" name="email" placeholder="Email" required>
+                        <input type="email" class="form-control" id="signup-email" name="email" placeholder="Email" autocomplete="email" required>
                     </div>
 
+                    <label for="signup-password">Password</label>
                     <div class="input-group form-group">
                         <div class="input-group-prepend">
                             <span class="input-group-text">
-                                <i class="fas fa-lock"></i>
+                                <i class="fas fa-lock" aria-hidden="true"></i>
                             </span>
                         </div>
-                        <input type="password" class="form-control" name="password" placeholder="Password" minlength="6" required>
+                        <input type="password" class="form-control" id="signup-password" name="password" placeholder="Password" autocomplete="new-password" minlength="6" required>
                     </div>
 
+                    <label for="signup-college">College Name</label>
                     <div class="input-group form-group">
                         <div class="input-group-prepend">
                             <span class="input-group-text">
-                                <i class="fas fa-university"></i>
+                                <i class="fas fa-university" aria-hidden="true"></i>
                             </span>
                         </div>
-                        <input type="text" class="form-control" name="college_name" placeholder="College Name" maxlength="150" required>
+                        <input type="text" class="form-control" id="signup-college" name="college_name" placeholder="College Name" maxlength="150" required>
                     </div>
 
-                    <div class="form-group">
-                        <span>I'm a</span>
-                        <input type="radio" class="ml-3" id="gender-male" name="gender" value="male" /> Male
+                    <fieldset class="form-group gender-options">
+                        <legend>I'm a</legend>
                         <label for="gender-male">
+                            <input type="radio" id="gender-male" name="gender" value="male" /> Male
                         </label>
-                        <input type="radio" class="ml-3" id="gender-female" name="gender" value="female" />
                         <label for="gender-female">
-                            Female
+                            <input type="radio" id="gender-female" name="gender" value="female" /> Female
                         </label>
-                    </div>
+                    </fieldset>
 
                     <div class="form-group">
                         <button type="submit" class="btn btn-block btn-primary">Create Account</button>
